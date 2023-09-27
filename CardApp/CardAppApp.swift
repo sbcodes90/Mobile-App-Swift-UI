01e822a8ba54884c29611113328647a682e1a2e4
sbcodes90 <sydneycburrell29@gmail.com>
2023-09-27 19:44:19 -0400

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by Sydney Burrell on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
