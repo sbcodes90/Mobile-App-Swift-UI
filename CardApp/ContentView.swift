6b00ee16108e02181b38b51108fb61845842951c
sbcodes90 <sydneycburrell29@gmail.com>
2023-09-27 20:43:51 -0400

    Learned how to make this from Chris Codes tutorial, using this as a basis to learn Swift and add on some custom features that will help me learn more about Swift

--- CardApp/ContentView.swift
+++ CardApp/ContentView.swift
@@ -9,16 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Testing")
+        ZStack {//allows you to place items on top of each other
+                    Image("background-plain")
+                        .resizable()//resizes image to fit the screen
+                        .ignoresSafeArea()//covers the full container of the iphone and ignores the safe area
+                    VStack {
+                        Spacer()
+                        Image("logo")
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Image("card7")
+                            Spacer()
+                            Image("card12")
+                            Spacer()
+                        }
+                        Spacer()
+                            Image("button")
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Text("Player")
+                                    .font(.headline)
+                                    .padding(.bottom, 10.0)
+                                Text("0")
+                                    .font(.largeTitle)
+                            }
+                            Spacer()
+                            VStack {
+                                Text("CPU")
+                                    .font(.headline)
+                                    .padding(.bottom, 10.0)
+                                Text("0")
+                                    .font(.largeTitle)
+                            }
+                            Spacer()
+                        }
+                        .foregroundColor(.white)//apply it to the Hstack and everything inside will do it
+                        Spacer()
+                    }
+                }
+            }
         }
-        .padding()
-    }
-}
-
+     
+//below is just for preview purposes does not get compiled
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
